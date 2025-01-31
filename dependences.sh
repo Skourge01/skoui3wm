7a779105cca306b18dd61bec10f3a60090cdc3bf
Skourge <oditorrinco222@gmail.com>
2025-01-31 09:17:01 -0300

    thunar release

--- dependences.sh
+++ dependences.sh
@@ -40,7 +40,7 @@ install_general_apps() {
         ttf-fira-code \
         dmenu \
         alacritty \
-        ranger \
+        thunar \
         nodejs \
         xorg-server \
         xorg-xinit \
